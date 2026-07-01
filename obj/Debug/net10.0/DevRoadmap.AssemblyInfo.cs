88c87951251d8c1c586e8a53fa373fe94a96681a
VanessaVilas <vanessavilas57@gmail.com>
2026-07-01 16:43:55 -0300

    Home - text

--- obj/Debug/net10.0/DevRoadmap.AssemblyInfo.cs
+++ obj/Debug/net10.0/DevRoadmap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevRoadmap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c867223e81b31b70e306d889eaac53c827931a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ead493638b9effac371af6f65593a9fdf70cd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevRoadmap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevRoadmap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
