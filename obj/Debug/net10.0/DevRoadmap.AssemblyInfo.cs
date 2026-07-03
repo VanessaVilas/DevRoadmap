90cb57e4a0ae7582d32c1b422f0cbbab0f96ffc8
VanessaVilas <vanessavilas57@gmail.com>
2026-07-03 17:42:42 -0300

    Home - button and font size

--- obj/Debug/net10.0/DevRoadmap.AssemblyInfo.cs
+++ obj/Debug/net10.0/DevRoadmap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevRoadmap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ead493638b9effac371af6f65593a9fdf70cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c87951251d8c1c586e8a53fa373fe94a96681a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevRoadmap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevRoadmap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
